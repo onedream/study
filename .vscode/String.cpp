c22d9f7def1f9d64b539d0b1e159323e640d3048
ONEDREAM <wangjunonedream@qq.com>
2022-12-30 13:04:19 +0800

    cpp string pratice

---
+++ .vscode/String.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    string s1,s2;
+    cin>>n>>s1;
+    s2=s1.substr(s1.length()-n,n);
+    s1.erase(s1.length()-n,n);
+    cout<<s2<<s1;
+    return 0;
+}
