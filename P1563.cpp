46f51329d3fb086748ace7b3fed46a4aaf3f0214
ONEDREAM <wangjunonedream@qq.com>
2023-01-06 15:14:12 +0800

    pratic

--- P1563.cpp
+++ P1563.cpp
@@ -1,49 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-typedef struct 
+typedef struct
 {
-    int x ;
-    char y [30];
+    int x;
+    char y[30];
     /* data */
-}toy;
+} toy;
 int main()
 {
-    int m,n,i=1,j=0,k=1,sum=0,b,c;
-    cin>>n>>m;
-    toy a[n+1];
-    while(i<=n){
-        cin>>a[i].x>>a[i].y;
+    int m, n, i = 1, j = 0, k = 1, sum = 0, b, c;
+    cin >> n >> m;
+    toy a[n + 1];
+    while (i <= n)
+    {
+        cin >> a[i].x >> a[i].y;
         i++;
     }
     // 规定逆时针为正，顺时针为负，向里右为正，左为负，向外右为负，左为正；
-    while(j<m){
-        cin>>b>>c;
-        if(a[k].x==0){
-            if(b==0){
-                k-=c;
+    while (j++ < m)
+    {
+        cin >> b >> c;
+        if (a[k].x == 0)
+        {
+            if (b == 0)
+            {
+                k -= c;
             }
-            else{
-                k+c;
+            else
+            {
+                k += c;
             }
-            k%=n;
-            if(k<=0){
-                k+=9;
-            }
-        }
-        else{
-            if(b==1){
-                k-=c;
-            }
-            else{
-                k+=c;
-            }
-            k%=n;
-            if(k<=0){
-                k+=9;
+            k %= n;
+            if (k <= 0)
+            {
+                k += n;
             }
         }
-        j++;
+        else
+        {
+            if (b == 1)
+            {
+                k -= c;
+            }
+            else
+            {
+                k += c;
+            }
+            k %= n;
+            if (k <= 0)
+            {
+                k += n;
+            }
+        }
     }
-    printf("%s",a[k].y);
+    printf("%s", a[k].y);
     return 0;
 }
